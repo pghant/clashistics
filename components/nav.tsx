064ed2fe6c309a582933245e016e65ef6de782dd
Prasant Ghantasala <g.prasant@outlook.com>
2023-09-27 23:21:01 -0400

    Make nav reactive

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,23 +2,26 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 type NavLinkProps = {
-    isActive: boolean;
     href: string;
     text: string
 }
 
-function NavLink({isActive, href, text}: NavLinkProps) {
+function NavLink({href, text}: NavLinkProps) {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
     return (
-        <li className="my-4 pl-2 md:my-0 md:pl-2 md:pr-1">
-            <a
-                className="active disabled:text-black/30 md:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+        <li className="my-2 pl-2 md:my-0 md:pl-0 md:pr-1">
+            <Link
+                className={`${isActive ? 'active' : ''} p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 md:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400`}
                 aria-current="page"
                 href={href}
             >
                 {text}
-            </a>
+            </Link>
         </li>
     );
 }
@@ -27,22 +30,20 @@ export default function Nav() {
     const [showNav, setShowNav] = useState(false);
 
     function handleNavToggle() {
-        console.log('toggle the menu');
         setShowNav(!showNav);
     }
 
     return (
         <nav
-            className="relative flex w-full flex-nowrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 md:flex-wrap md:justify-start md:py-4"
-        >
+            className="relative flex w-full flex-nowrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 md:flex-wrap md:justify-start md:py-4">
             <div className="flex w-full flex-wrap items-center justify-between px-3">
-                <div className="ml-2">
+                <div className="ml-2 md:mr-2">
                     <Link className="text-xl text-neutral-800 dark:text-neutral-200" href="/">Clashistics</Link>
                 </div>
                 <button
                     className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 md:hidden"
                     type="button"
-                    aria-expanded="false"
+                    aria-expanded={showNav}
                     aria-label="Toggle navigation"
                     onClick={handleNavToggle}
                 >
@@ -61,33 +62,11 @@ export default function Nav() {
                         </svg>
                     </span>
                 </button>
-                <div className="visible mt-2 hidden flex-grow basis-[100%] items-center md:mt-0 md:flex md:basis-auto">
+                <div
+                    className={`${showNav ? '' : 'hidden'} mt-2 flex-grow basis-[100%] items-center md:mt-0 md:flex md:basis-auto`}>
                     <ul className="list-style-none mr-auto flex flex-col pl-0 md:mt-1 md:flex-row">
-                        <li className="my-4 pl-2 md:my-0 md:pl-2 md:pr-1">
-                            <a
-                                className="active disabled:text-black/30 md:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                                aria-current="page"
-                                href="#"
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li className="mb-4 pl-2 md:mb-0 md:pl-0 md:pr-1">
-                            <a
-                                className="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 md:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                                href="#"
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li className="mb-4 pl-2 md:mb-0 md:pl-0 md:pr-1">
-                            <a
-                                className="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 md:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                                href="#"
-                            >
-                                Pricing
-                            </a>
-                        </li>
+                        <NavLink href="/" text="Home"/>
+                        <NavLink href="/about" text="About"/>
                     </ul>
                 </div>
             </div>
