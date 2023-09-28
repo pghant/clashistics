b1acd264fed055f3f9c155c6abee88b9e463dd32
Prasant Ghantasala <g.prasant@outlook.com>
2023-09-28 14:57:55 -0400

    Add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import Nav from '@components/nav';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Clashistics',
-    description: 'Track your Clash of Clans',
-}
+  title: 'Clashistics',
+  description: 'Track your Clash of Clans',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-    <Nav/>
-    {children}
-    </body>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        This is the main page
+      This is the main page
     </main>
-  )
+  );
 }
